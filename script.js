3337b2d1f5beaf844cb7fe1006714f13bd96a93e
Cesar Laviery <141261537+cesarlaviery@users.noreply.github.com>
2024-04-15 08:56:00 -0400

    update data

--- script.js
+++ script.js
@@ -2,22 +2,20 @@
 
 const showPrize = document.querySelectorAll(".important-moments--show");
 const hidePrize = document.querySelectorAll(".important-moments--hide");
-const informationImportantMoments = document.querySelectorAll(".important-moments__information");
+const ImportantMoments = document.querySelectorAll(".important-moments__information");
+
+const incrementClassList = (nodeList, classList) => {
+    for (element of nodeList) {
+        classList.push(element);
+    };
+};
 
 let showClassList = [];
-for (element of showPrize) {
-    showClassList.push(element);
-};
-
+incrementClassList(showPrize, showClassList);
 let hideClassList = [];
-for (element of hidePrize) {
-    hideClassList.push(element);
-};
-
-let informationImportantMomentsClassList = [];
-for (element of informationImportantMoments) {
-    informationImportantMomentsClassList.push(element);
-};
+incrementClassList(hidePrize, hideClassList);
+let ImportantMomentsClassList = [];
+incrementClassList(ImportantMoments, ImportantMomentsClassList);
 
 showClassList.forEach((element, index) => {
     showClassList[index].addEventListener("click", () => {
@@ -34,7 +32,7 @@ hideClassList.forEach((element, index) => {
 const show = (showElementList, hideElementListNum) => {
     showClassList.forEach((element, index) => {
         if (showElementList == showClassList[index]) {
-            informationImportantMomentsClassList[index].classList.add("important-moments__information--opacity");
+            ImportantMomentsClassList[index].classList.add("important-moments__information--opacity");
             showClassList[index].classList.add("important-moments--show-opacity");
             hideClassList[index].classList.add("important-moments--hide-opacity");
         };
@@ -44,7 +42,7 @@ const show = (showElementList, hideElementListNum) => {
 const hide = (hideElementList, hideElementListNum) => {
     hideClassList.forEach((element, index) => {
         if (hideElementList == hideClassList[index]) {
-            informationImportantMomentsClassList[index].classList.remove("important-moments__information--opacity");
+            ImportantMomentsClassList[index].classList.remove("important-moments__information--opacity");
             showClassList[index].classList.remove("important-moments--show-opacity");
             hideClassList[index].classList.remove("important-moments--hide-opacity");
         };
@@ -57,18 +55,18 @@ window.addEventListener("scroll", () => {
     const phrasesAnimation = document.querySelectorAll(".phrases");
 
     let phrasesClassList = [];
-    for(element of phrasesAnimation) {
+    for (element of phrasesAnimation) {
         phrasesClassList.push(element);
     };
 
     for (let i = 0; i < phrasesClassList.length; i++) {
         const phrasesAnimationPosition = phrasesClassList[i].getBoundingClientRect().top;
-    const screenSize = window.innerHeight/1.1;
+        const screenSize = window.innerHeight / 1.1;
 
-    if(phrasesAnimationPosition < screenSize) {
-        phrasesAnimation[i].style = "animation: show 1s ease-out both"
-    } else {
-        phrasesAnimation[i].style = "animation: hide 1s ease-out both"
-    }
+        if (phrasesAnimationPosition < screenSize) {
+            phrasesAnimation[i].style = "animation: show 1s ease-out both"
+        } else {
+            phrasesAnimation[i].style = "animation: hide 1s ease-out both"
+        }
     }
 });
